aae5154f24dff7fcfec6442dd35818f4d0471751
edlectrico <edlectrico@gmail.com>
2015-08-12 12:49:46 +0200

    Added some preprocessing tools for the tweets.

--- tweets_sentiment_analysis/test_tweet.py
+++ tweets_sentiment_analysis/test_tweet.py
@@ -1,4 +1,5 @@
 import sentiment_mod as s
+import tweet_preprocessor as prep
 
-print(s.sentiment("I hate all minions"))
-print(s.sentiment("I love minions"))
+print(s.sentiment(prep.processTweet("I haaaaate all minions!! I better kill time watching www.youtube.com")))
+print(s.sentiment(prep.processTweet("I love minions")))
---
+++ tweets_sentiment_analysis/tweet_preprocessor.py
@@ -0,0 +1,18 @@
+import re
+
+def replaceTwoOrMore(s):
+    #look for 2 or more repetitions of character and replace with the character itself
+    pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
+    return pattern.sub(r"\1\1", s)
+
+def processTweet(tweet):
+    tweet = replaceTwoOrMore(tweet)
+    tweet = tweet.lower()
+    tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+    tweet = re.sub('@[^\s]+','AT_USER',tweet)
+    tweet = re.sub('[\s]+', ' ', tweet)
+    tweet = re.sub(r'#([^\s]+)', r'\1', tweet)
+    tweet = tweet.strip('\'"')
+    print (tweet)
+    return tweet
+
