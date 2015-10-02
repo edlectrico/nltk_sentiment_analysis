849ea4e22ad3841ed8be6899faae3ec5f21ca053
edlectrico <edlectrico@gmail.com>
2015-10-02 10:00:23 +0200

    Including utf8 coding line

--- tweets_sentiment_analysis/tweet_preprocessor.py
+++ tweets_sentiment_analysis/tweet_preprocessor.py
@@ -1,12 +1,13 @@
+# -*- coding: utf-8 -*-
 import re
 
-def replaceTwoOrMore(s):
+def replace_two_or_more(s):
     #look for 2 or more repetitions of character and replace with the character itself
     pattern = re.compile(r"(.)\1{1,}", re.DOTALL)
     return pattern.sub(r"\1\1", s)
 
-def processTweet(tweet):
-    tweet = replaceTwoOrMore(tweet)
+def process_tweet(tweet):
+    tweet = replace_two_or_more(tweet)
     tweet = tweet.lower()
     tweet = re.sub('((www\.[^\s]+)|(https?://[^\s]+))',' ',tweet)
     tweet = re.sub('@[^\s]+',' ',tweet)
