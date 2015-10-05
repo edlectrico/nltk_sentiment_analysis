ba3324efdcf15f7464c06ac1675f1e9d70ae5df6
edlectrico <edlectrico@gmail.com>
2015-10-05 16:26:48 +0200

    Added csvs

--- tweets_sentiment_analysis/volkswagen/vw_geolocate_tweets.py
+++ tweets_sentiment_analysis/volkswagen/vw_geolocate_tweets.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import sys
-
-# import language_detector as lang_detector
+import re
 
 # Input DataFrames
 vw_original = pd.read_csv('output/vw_clean_en_rechecked.csv')
@@ -10,18 +9,22 @@ locations = pd.read_csv('../data/cities_geo.csv')
 vw = pd.DataFrame(data=vw_original)
 cities = pd.DataFrame(data=locations)
 # Output DataFrame
-vw_geo = pd.DataFrame(columns=['id', 'latitude', 'longitude'])
+vw_geo = pd.DataFrame(columns=['id', 'location', 'latitude', 'longitude'])
 
 for c_index, c_row in cities.iterrows():
   try:
     city = str(c_row['city']).lower().replace(" ", "") # cities may be written in a different way in each dataset
-    print city
+    clean_city = re.sub('\W+','', city)
+    print clean_city
     for vw_index, vw_row in vw.iterrows():
-      if city in str(vw_row['location']).lower().replace(" ", ""):
+      vw_city = str(vw_row['location']).lower().replace(" ", "")
+      clean_vw_city = re.sub('\W+','', vw_city)
+      if clean_city in clean_vw_city:
         print 'FOUND!\n'
 	# Now we append a new line to the output DataFrame containing
 	# the tweet id for future JOINS and the coordinates of the tweet
         vw_geo = vw_geo.append({'id':str(vw_row['id']), 
+			'location':c_row['city'],
 			'latitude':c_row['lat'], 
 			'longitude':c_row['lon']}, 
 			ignore_index=True)
