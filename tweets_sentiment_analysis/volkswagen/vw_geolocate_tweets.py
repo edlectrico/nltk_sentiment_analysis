f5f0f0b4aa4150839418aa12589cb4051891b15d
edlectrico <edlectrico@gmail.com>
2015-10-06 16:05:07 +0200

    Updating some lines...

--- tweets_sentiment_analysis/volkswagen/vw_geolocate_tweets.py
+++ tweets_sentiment_analysis/volkswagen/vw_geolocate_tweets.py
@@ -12,16 +12,18 @@ cities = pd.DataFrame(data=locations)
 # Output DataFrame
 vw_geo = pd.DataFrame(columns=['id', 'location', 'country_code', 'latitude', 'longitude'])
 
+cities_found = 0
+
 for c_index, c_row in cities.iterrows():
   try:
     city = str(c_row['city']).lower().replace(" ", "") # cities may be written in a different way in each dataset
     clean_city = re.sub('\W+','', city)
-    print(clean_city)
     for vw_index, vw_row in vw.iterrows():
       vw_city = str(vw_row['location']).lower().replace(" ", "")
       clean_vw_city = re.sub('\W+','', vw_city)
       if clean_city in clean_vw_city:
-        print('FOUND!\n')
+        cities_found += 1
+        print('Cities found:', cities_found)
 	# Now we append a new line to the output DataFrame containing
 	# the tweet id for future JOINS and the coordinates of the tweet
         vw_geo = vw_geo.append({'id':str(vw_row['id']), 
@@ -32,10 +34,9 @@ for c_index, c_row in cities.iterrows():
 			ignore_index=True)
 
   except KeyboardInterrupt:
-    print('KeyboradInterrupt exception raised: Generating out file...')
-    # print 'Writing to file'
+    print('KeyboradInterrupt exception raised: Generating output/vw_clean_en_geo.csv')
     vw_geo.to_csv('output/vw_clean_en_geo.csv')
     sys.exit()
 
-print('Writing to file')
+print('Writing to output/vw_clean_en_geo.csv')
 vw_geo.to_csv('output/vw_clean_en_geo.csv')
